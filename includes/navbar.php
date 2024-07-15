27533cf69c535041bb70ee535e8d6ad5f99de9b6
GPM_DEV <pedrohenrique10001@gmail.com>
2024-07-15 17:23:33 -0300

    Construção da Navbar

---
+++ includes/navbar.php
@@ -0,0 +1,23 @@
+<div class="container" id="nav-container">
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <a href="#" class="navbar-brand">
+            <img id="logo" src="../assets/img/logo.svg" alt="GPM Auto"><span class="brand-text">GPM AUTO</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links" aria-controls="#navbar-links" aria-expanded="false" aria-label="Toggle-navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
+            <div class="navbar-nav">
+                <a class="navi-tem nav-link" id="inicio-menu" href="#">Início</a>
+                <a class="navi-tem nav-link" id="add-menu" href="#">Adicionar usuário</a>
+                <a class="navi-tem nav-link" id="materials-menu" href="#">Gerenciar materiais</a>
+                <a class="navi-tem nav-link" id="sell-menu" href="#">Adicionar Venda</a>
+                <a class="navi-tem nav-link" id="service-menu" href="#">Adicionar serviço</a>
+                <button type="button" class="btn btn-primary" id="sair">
+                <img src="../assets/img/icon.svg" alt="out">Sair 
+                </button>
+            </div>
+        </div>
+    </nav>
+</div>
